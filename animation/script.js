fb9de6056396fd0c39ee474ddd8771854a28f83d
mistermax03 <mister.max0303@gmail.com>
2024-04-05 10:55:18 +0200

    animation

---
+++ animation/script.js
@@ -0,0 +1,131 @@
+const colors = [
+                "rgb(255,0,0)",
+                "rgb(255,255,0)",
+                "rgb(255,0,255)",
+                "rgb(255,255,255)",
+                "rgb(0,0,0)",
+            ];
+
+
+window.addEventListener("load", () => {
+    window.onresize = () => createGrid();
+    createGrid();
+});
+
+const grid = document.getElementById("tiles");
+let columns = Math.floor(document.body.clientWidth / 50);
+let rows = Math.floor(document.body.clientHeight / 50);
+
+let gridArray;
+const delay = 500;
+
+const Corners = {
+    TOPRIGHT: 12,
+    TOPLEFT : 14,
+    BOTTOMRIGHT : 19,
+    BOTTOMLEFT  : 20,
+};
+class Tile {
+    constructor(tile, timeoutID, state) {
+        this.tile = tile;
+        this.timeoutID = timeoutID;
+        this.state = state;
+    }
+}
+
+const tileHandler = (x, y) => {
+    return (event) => {
+    console.log("clic");
+    changeColor(x, y);
+    setTimeout(propager, delay, x, y);
+    }
+}
+
+const changeColor = (x, y) => {
+    let t = gridArray[y][x];
+    if (t.state < 5) {
+        console.log("colorchange")
+        t.tile.style.backgroundColor = colors[t.state++];
+        setTimeout(changeColor, delay, x, y);
+    } else {
+        t.state= 0;
+    }
+}
+
+const inbounds = (x,y)  => {
+    return ((x >= 0 && x <= columns) && (y >= 0 && y<= rows))
+}
+
+const propager = (x,y) => {
+    lineColorChange(x,y, 1, 0);  
+    lineColorChange(x,y, -1, 0);  
+    lineColorChange(x,y, 0, 1);   
+    lineColorChange(x,y, 0, -1);  
+    if (inbounds(x+1, y+1)) propagerCorner(x+1, y+1, Corners.BOTTOMRIGHT);
+    if (inbounds(x+1, y-1)) propagerCorner(x+1, y-1, Corners.TOPRIGHT);
+    if (inbounds(x-1, y+1)) propagerCorner(x-1, y+1, Corners.BOTTOMLEFT);
+    if (inbounds(x-1, y-1)) propagerCorner(x-1, y-1, Corners.TOPLEFT);
+}
+
+const propagerCorner =  (x, y, corner) => {
+    switch (corner) {
+        case Corners.TOPLEFT :
+            if (inbounds(x-1, y+1)) propagerCorner(x-1, y+1, Corners.TOPLEFT);
+            lineColorChange(x, y, -1, 0);
+            lineColorChange(x, y, 0, 1)
+            break;
+        case Corners.TOPRIGHT : 
+            if (inbounds(x+1, y+1)) propagerCorner(x+1, y+1, Corners.TOPRIGHT);
+            lineColorChange(x, y, 1, 0);
+            lineColorChange(x, y, 0, 1)
+            break;
+        case Corners.BOTTOMLEFT :
+            if (inbounds(x-1, y-1)) propagerCorner(x-1, y-1, Corners.BOTTOMLEFT);
+            lineColorChange(x, y, -1, 0);
+            lineColorChange(x, y, 0, -1)
+            break;
+        case Corners.BOTTOMRIGHT :
+            if (inbounds(x+1, y-1)) propagerCorner(x+1, y-1, Corners.BOTTOMRIGHT);
+            lineColorChange(x, y, 1, 0);
+            lineColorChange(x, y, 0, -1)
+            break;
+    }
+}
+
+const lineColorChange = (x, y, dirx, diry) => {
+    if (inbounds(x+dirx, y+diry)) {
+        changeColor(x+dirx, y+diry);
+        setTimeout(lineColorChange, delay, x+dirx, y+diry, dirx, diry);
+    }
+
+}
+
+
+const createGrid = () => {
+    grid.innerHTML = "";
+
+    columns = Math.floor(document.body.clientWidth / 50);
+    rows = Math.floor(document.body.clientHeight / 50);
+    
+    gridArray = new Array(rows);
+    
+    for(let y=0; y< rows; y++) {
+        let line = grid.appendChild(document.createElement("tr"));
+        gridArray[y] = new Array(columns);
+        for(let x=0; x< columns; x++) {
+            let tile = line.appendChild(document.createElement("td"));
+            tile.classList.add("tile");
+            tile.dataset.x = x;
+            tile.dataset.y = y;
+            gridArray[y][x] = new Tile(tile, 0, 0);
+            tile.addEventListener("click", tileHandler(x,y));
+        }
+    }
+
+    console.log(gridArray)
+
+}
+
+
+
+
