2e0f4e85f62b4857e437931dace05d65343da89a
mistermax03 <mister.max0303@gmail.com>
2024-04-08 14:11:59 +0200

    Update script.js

--- animation/script.js
+++ animation/script.js
@@ -16,14 +16,13 @@ class Color {
 const fade = (from, target, nbcolors) => {
     let r = new Array(nbcolors);
     for (let i = 1; i <= nbcolors; i++) {
-        r[i-1] = `rgb(${from.red+((target.red-from.red)/nbcolors*i)},${from.green+((target.green-from.green)/nbcolors*i)},${from.blue+((target.blue-from.blue)/nbcolors*i)})`
+        r[i-1] = `rgb(${Math.floor(from.red+((target.red-from.red)/nbcolors*i))},${Math.floor(from.green+((target.green-from.green)/nbcolors*i))},${Math.floor(from.blue+((target.blue-from.blue)/nbcolors*i))})`;
     }
     return r;
 }
 
 let from = new Color(0,0,0);
 let target = new Color(255,255,255);
-let colors = fade(from, target, 6);
 
 
 const grid = document.getElementById("tiles");
@@ -52,26 +51,27 @@ class Tile {
     constructor(tile) {
         this.tile = tile;
         this.state = 0;
+        this.colors = null;
     }
 }
 
 const tileHandler = (x, y) => {
     return (event) => {
+        let colors = fade(from, target, 6);
         from = target;
         target = new Color(Math.floor(Math.random()*256),Math.floor(Math.random()*256),Math.floor(Math.random()*256));
-        
-        colors = fade(from, target, 6);
-        changeColor(x, y);
-        setTimeout(propager, delay, x, y);
+
+        changeColor(x, y, colors);
+        setTimeout(propager, delay, x, y, colors);
     }
 }
 
-const changeColor = (x, y) => {
+const changeColor = (x, y, colors, first=true) => {
     let t = gridArray[y][x];
-    
-    if (t.state < colors.length) {
-        t.tile.style.backgroundColor = colors[t.state++];
-        setTimeout(changeColor, colorChangeDelay, x, y);
+    if (first) t.colors = colors;
+    if (t.state < t.colors.length) {
+        t.tile.style.backgroundColor = t.colors[t.state++];
+        setTimeout(changeColor, colorChangeDelay, x, y, colors, false);
     } else {
         t.state = 0;
     }
@@ -82,50 +82,50 @@ const inbounds = (x,y)  => {
     return ((x >= 0 && x < columns) && (y >= 0 && y< rows))
 }
 
-const propager = (x,y) => {
-    lineColorChange(x,y, 1, 0);  
-    lineColorChange(x,y, -1, 0);  
-    lineColorChange(x,y, 0, 1);   
-    lineColorChange(x,y, 0, -1);  
+const propager = (x,y, colors) => {
+    lineColorChange(x,y, 1, 0, colors);  
+    lineColorChange(x,y, -1, 0, colors);  
+    lineColorChange(x,y, 0, 1, colors);   
+    lineColorChange(x,y, 0, -1, colors);  
 
-    if (inbounds(x+1, y+1)) setTimeout(propagerCorner, delay, x+1, y+1, Corners.BOTTOMRIGHT);
-    if (inbounds(x-1, y-1)) setTimeout(propagerCorner, delay, x-1, y-1, Corners.TOPLEFT);
+    if (inbounds(x+1, y+1)) setTimeout(propagerCorner, delay, x+1, y+1, Corners.BOTTOMRIGHT, colors);
+    if (inbounds(x-1, y-1)) setTimeout(propagerCorner, delay, x-1, y-1, Corners.TOPLEFT, colors);
 
-    if (inbounds(x+1, y-1)) setTimeout(propagerCorner, delay, x+1, y-1, Corners.TOPRIGHT);
-    if (inbounds(x-1, y+1)) setTimeout(propagerCorner, delay, x-1, y+1, Corners.BOTTOMLEFT);
+    if (inbounds(x+1, y-1)) setTimeout(propagerCorner, delay, x+1, y-1, Corners.TOPRIGHT, colors);
+    if (inbounds(x-1, y+1)) setTimeout(propagerCorner, delay, x-1, y+1, Corners.BOTTOMLEFT, colors);
     
 }
 
-const propagerCorner =  (x, y, corner) => {
-    changeColor(x, y);
+const propagerCorner =  (x, y, corner, colors) => {
+    changeColor(x, y, colors);
     switch (corner) {
         case Corners.TOPLEFT :
-            setTimeout(lineColorChange, delay, x, y, -1, 0);
-            setTimeout(lineColorChange, delay, x, y, 0, -1);
-            if (inbounds(x-1, y-1)) setTimeout(propagerCorner, delay*2, x-1, y-1, Corners.TOPLEFT);
+            setTimeout(lineColorChange, delay, x, y, -1, 0, colors);
+            setTimeout(lineColorChange, delay, x, y, 0, -1, colors);
+            if (inbounds(x-1, y-1)) setTimeout(propagerCorner, delay*2, x-1, y-1, Corners.TOPLEFT, colors);
             break;
         case Corners.TOPRIGHT : 
-            setTimeout(lineColorChange, delay, x, y, 1, 0);
-            setTimeout(lineColorChange, delay, x, y, 0, -1);
-            if (inbounds(x+1, y-1)) setTimeout(propagerCorner, delay*2, x+1, y-1, Corners.TOPRIGHT);
+            setTimeout(lineColorChange, delay, x, y, 1, 0, colors);
+            setTimeout(lineColorChange, delay, x, y, 0, -1, colors);
+            if (inbounds(x+1, y-1)) setTimeout(propagerCorner, delay*2, x+1, y-1, Corners.TOPRIGHT, colors);
             break;
         case Corners.BOTTOMLEFT :
-            setTimeout(lineColorChange, delay, x, y, -1, 0);
-            setTimeout(lineColorChange, delay, x, y, 0, 1);
-            if (inbounds(x-1, y+1)) setTimeout(propagerCorner, delay*2, x-1, y+1, Corners.BOTTOMLEFT);
+            setTimeout(lineColorChange, delay, x, y, -1, 0, colors);
+            setTimeout(lineColorChange, delay, x, y, 0, 1, colors);
+            if (inbounds(x-1, y+1)) setTimeout(propagerCorner, delay*2, x-1, y+1, Corners.BOTTOMLEFT, colors);
             break;
         case Corners.BOTTOMRIGHT :
-            setTimeout(lineColorChange, delay, x, y, 1, 0);
-            setTimeout(lineColorChange, delay, x, y, 0, 1);
-            if (inbounds(x+1, y+1)) setTimeout(propagerCorner, delay*2, x+1, y+1, Corners.BOTTOMRIGHT);
+            setTimeout(lineColorChange, delay, x, y, 1, 0, colors);
+            setTimeout(lineColorChange, delay, x, y, 0, 1, colors);
+            if (inbounds(x+1, y+1)) setTimeout(propagerCorner, delay*2, x+1, y+1, Corners.BOTTOMRIGHT, colors);
             break;
     }
 }
 
-const lineColorChange = (x, y, dirx, diry) => {
+const lineColorChange = (x, y, dirx, diry, colors) => {
     if (inbounds(x+dirx, y+diry)) {
-        changeColor(x+dirx, y+diry);
-        setTimeout(lineColorChange, delay, x+dirx, y+diry, dirx, diry);
+        changeColor(x+dirx, y+diry, colors);
+        setTimeout(lineColorChange, delay, x+dirx, y+diry, dirx, diry, colors);
     }
 
 }
@@ -155,9 +155,6 @@ const createGrid = () => {
             // tile.innerText = `${x}-${y}`
         }
     }
-
-    console.log(gridArray)
-
 }
 
 
